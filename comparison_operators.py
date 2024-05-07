5995b7e483b0c473065381ff9f668f9879943174
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-07 11:03:22 +0530

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,21 @@
+# comparison operators
+a=4
+b=5
+
+# equal
+print("a==b:",a==b)
+
+# not equal
+print("a!=b:",a!=b)
+
+# greater than
+print("a>b:",a>b)
+
+# less than
+print("a<b:",a<b)
+
+# greater than or equal to
+print("a>=b:",a>=b)
+
+# lesser than or equal to
+print("a<=b:",a<=b)
